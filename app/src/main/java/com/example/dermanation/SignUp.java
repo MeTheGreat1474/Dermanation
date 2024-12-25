ff76494ab001095fdffea329966acde977497598
adamarbain <u2101735@siswa.um.edu.my>
2024-12-25 14:58:29 +0800

    Sign In and Sign Up Functioning

---
+++ app/src/main/java/com/example/dermanation/SignUp.java
@@ -0,0 +1,105 @@
+package com.example.dermanation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+
+    Button buttonReg;
+
+    FirebaseAuth mAuth;
+
+    TextView textView;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        buttonReg = findViewById(R.id.btn_register);
+        textView = findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(SignUp.this, "Enter email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(SignUp.this, "Enter password", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+
+                                    Toast.makeText(
+                                            SignUp.this,
+                                            "Authentication created.",
+                                            Toast.LENGTH_SHORT
+                                    ).show();
+                                    Intent intent = new Intent(getApplicationContext(), SignIn.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    Toast.makeText(
+                                            SignUp.this,
+                                            "Authentication failed.",
+                                            Toast.LENGTH_SHORT
+                                    ).show();
+                                }
+                            }
+                        });
+
+            }
+        });
+    }
+}
